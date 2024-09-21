daca1d14825147e9341203d5b86eb83190250aaf
PUNEET VAISHNAV <65516671+iamfutureflash@users.noreply.github.com>
2024-09-22 00:21:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <h1 className="text-3xl font-bold underline flex justify-center items-center text-gray-600 bg-black h-screen">
+      Hello world! Puneet
+    </h1>
+  )
+}
+
+export default App
